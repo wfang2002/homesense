f3e11a40c3d0e8e95fc2751012dbcd150f316fde
Wei Fang <wei.fang@bchydro.com>
2013-07-18 14:27:16 -0700

    add prev/next buttons

--- web/client/main.js
+++ web/client/main.js
@@ -1,5 +1,9 @@
 Meteor.subscribe("motion_sensor_events");
 
+var hourlyChart;
+var hourlySerialData = [1];
+var hourlyLabel = ["0"];
+var hourlyChartEnd = new Date();
 
 Template.home.motionEvents = function() {
     return MotionSensorEvents.find({status:'1'}, {sort:{updated:-1}, limit:5});
@@ -36,8 +40,41 @@ Template.home.hourlyEvents = function() {
 }
 
 Template.home.rendered = function() {
+//    if (!hourlyChart) {
+//        createHourlyChart();
+//    } else {
+//        refreshHourlyChart();
+//    }
     showHourlyChart();
+
+    // Force refresh jQuery Mobile elements
+    $('#home-content').trigger('create');
 }
+
+
+Template.home.events({
+    'click #btn-prev':function(evt, tmpl) {
+        evt.stopPropagation();
+        evt.preventDefault();
+
+        var now = new Date();
+        var next = hourlyChartEnd.getTime() - 24*60*60000;
+        if (next > now.getTime()) next = now.getTime;
+        hourlyChartEnd = new Date(next);
+        showHourlyChart();
+    },
+
+    'click #btn-next':function(evt, tmpl) {
+        evt.stopPropagation();
+        evt.preventDefault();
+
+        var now = new Date();
+        var next = hourlyChartEnd.getTime() + 24*60*60000;
+        if (next > now.getTime()) next = now.getTime();
+        hourlyChartEnd = new Date(next);
+        showHourlyChart();
+    }
+})
 
 Template.home.helpers({
     shortTime: function(time) {
@@ -75,11 +112,86 @@ function getHourlyStat(tsStart, tsEnd) {
     return list;
 }
 
-function showHourlyChart() {
+function createHourlyChart() {
+    console.log('create hourly chart');
+    hourlyChart = $.jqplot('hourly-chart', [hourlySerialData], {
+        // The "seriesDefaults" option is an options object that will
+        // be applied to all series in the chart.
+        seriesDefaults:{
+            renderer:$.jqplot.BarRenderer,
+            rendererOptions: {
+                barMargin: 2,
+                fillToZero: true
+            }
+        },
+        // Custom labels for the series are specified with the "label"
+        // option on the series option.  Here a series option object
+        // is specified for each series.
+        series:[
+            {label:'Event Count'}
+        ],
+        // Show the legend and put it outside the grid, but inside the
+        // plot container, shrinking the grid to accomodate the legend.
+        // A value of "outside" would not shrink the grid and allow
+        // the legend to overflow the container.
+        legend: {
+            show: true,
+            placement: 'insideGrid'
+        },
+        axes: {
+            // Use a category axis on the x axis and use our custom ticks.
+            xaxis: {
+                renderer: $.jqplot.CategoryAxisRenderer,
+                ticks: hourlyLabel
+            },
+            // Pad the y axis just a little so bars can get close to, but
+            // not touch, the grid boundaries.  1.2 is the default padding.
+            yaxis: {
+                //pad: 1.05,
+                padMin: 0,
+                tickOptions: {formatString: '%d'}
+            }
+        } ,
 
+        axesDefaults: {
+            tickOptions: {
+                showGridline: false
+            }
+
+        }
+    });
+
+}
+
+function refreshHourlyChart() {
+    console.log('refresh hourly chart');
     var now = new Date();
     var yesterDay = new Date(now.getTime() - 24*60*60000);
     var hourlyData = getHourlyStat(yesterDay, now);
+
+    hourlySerialData = _.map(hourlyData, function(val){return val.count;});
+    hourlyLabel = _.map(hourlyData, function(val){
+        var date = new Date(val.ts);
+        if (date.getHours() == 0) {
+            return date.format('MM/dd');
+        } else {
+            if (date.getHours() % 2 === 0) {
+                return date.format('hh');
+            } else {
+                return "";
+            }
+        }
+    });
+
+    hourlyChart.series[0].data = hourlySerialData;
+    hourlyChart.replot();
+}
+
+
+function showHourlyChart() {
+
+    var tsStart = new Date(hourlyChartEnd.getTime() - 24*60*60000);
+    var hourlyData = getHourlyStat(tsStart, hourlyChartEnd);
 
     var s1 = _.map(hourlyData, function(val){return val.count;});
     var ticks = _.map(hourlyData, function(val){
@@ -95,7 +207,14 @@ function showHourlyChart() {
         }
     });
 
-    var plot1 = $.jqplot('hourly-chart', [s1], {
+//    if (hourlyChart) {
+//        hourlyChart.replot();
+//        return;
+//    }
+    $('#hourly-chart').empty();
+
+
+    hourlyChart = $.jqplot('hourly-chart', [s1], {
         // The "seriesDefaults" option is an options object that will
         // be applied to all series in the chart.
         seriesDefaults:{
