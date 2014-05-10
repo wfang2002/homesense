05cf5884ea63b2d25f1a6321c7465bf2f5409e52
Wei Fang <wfang2002@gmail.com>
2014-05-09 21:10:09 -0700

    imple uartdimmer

--- controller/raspberrypi/src/uartdimmer.js
+++ controller/raspberrypi/src/uartdimmer.js
@@ -1,8 +1,48 @@
+var cmdQueue = [];
+var busy = false;
+
+var SerialPort = require("serialport").SerialPort
+var serialPort = new SerialPort("/dev/ttyAMA0", {
+  baudrate: 57600
+});
+
+
+serialPort.on("open", function () {
+  console.log('port open');
+  serialPort.on('data', function(data) {
+    console.log('data received: ' + data);
+
+    if (cmdQueue.length > 0) {
+        var cmd = cmdQueue.shift();
+        serialPort.write(cmd);
+    }
+    busy = false;
+  });
+
+  setInterval(function(){
+	// clear stuck commands to avoid memory overflow
+	if (cmdQueue.length > 10)cmdQueue = [];
+  }, 60000);
+
+});
 
 
 function setLevels(levels) {
 	console.log("Entering uartdimmer.setLevels()");
+    
+    if (busy) {
+        cmdQueue.push("L " + levels.join(',') + "\n");
+        return;
+    }
+    busy = true;
+    serialPort.write("L " + levels.join(',') + "\n", function(err, results) {
+        if (err)console.log('err ' + err);
+	else console.log("command sent");
+
+        //console.log('results ' + results);
+    });
 }
 
 
 exports.setLevels = setLevels;
+
